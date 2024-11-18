717212e85c927df3d8d191ecebd0076394ec3591
sajedul2022 <sajedul.idb.info@gmail.com>
2024-11-18 17:14:23 +0600

    Express-MongoDB reomte Crud

---
+++ 5. Mern/express-mongodb-pratice/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res) => res.send("From server Hello World!"));
+// acecpt JSON
+app.use(express.json());
+// static file route
+app.use(express.static(__dirname));
+//  send file or image
+app.get("/getFile", (req, res) => {
+  res.sendFile(path.join(__dirname, "upload", "sc.jpg"));
+});
+
+// const port = 8000;
+// app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+// const url = "mongodb://127.0.0.1:27017/";
+const url = 'mongodb+srv://sajedul:test-12345@express.fqblq.mongodb.net/Test?retryWrites=true&w=majority&connectTimeoutMS=30000';
+
+mongoose
+  .connect(url, { connectTimeoutMS: 30000 })
+  .then(() => {
+    console.log("Database connected...");
+    app.listen(8000, () => {
+      console.log("Server is running...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
+
+
+// User Route 
+const userRouter = require("./router/userRouter");
+app.use("/api/user", userRouter);
