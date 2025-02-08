1d574d67412fd5bdc6bd98543ca1182eb9e55cce
sajedul2022 <sajedul.idb.info@gmail.com>
2025-02-08 15:50:19 +0600

    Nest- microservice, rabbitMQ initial

---
+++ 05. MERN/nestjs-microservices/http-api-gateway/src/main.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable prettier/prettier */
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () => 
+    console.log(`Server is running on port ${PORT}`));
+}
+bootstrap();
